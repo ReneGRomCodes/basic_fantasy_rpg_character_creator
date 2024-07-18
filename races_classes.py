208a95542fb12671780e76e7c9a03aa0130cbb68
René Romero <rene.romero.2517@gmail.com>
2024-07-18 12:22:51 +0200

    Added 'classes_description.txt' with detailed description for playable classes and implemented function 'show_class_description()' in 'races_classes.py'.

--- races_classes.py
+++ races_classes.py
@@ -1,8 +1,17 @@
 # Race descriptions.
-
 def show_race_descriptions():
-    """Print detailed description of playable races from race_description.txt."""
+    """Print detailed description of playable races from 'races_description.txt'."""
     file = "races_description.txt"
+    with open(file) as f:
+        for line in f:
+            f.readline()
+            print(line)
+
+
+# Class descriptions.
+def show_class_descriptions():
+    """Print detailed description of playable classes from 'classes_description'.txt."""
+    file = "classes_description.txt"
     with open(file) as f:
         for line in f:
             f.readline()
