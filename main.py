1c623f1c41dcc5b4e594835593ef4be9f4080664
René Romero <rene.romero.2517@gmail.com>
2024-08-02 16:39:48 +0200

    minor change to import statement and implementation.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import main_functions as mf
-from functions import check_yes_no as yes_no
+from functions import check_yes_no
 import character_object as char
 """Main module for the 'Basic Fantasy RPG Character Creator'. This module serves as the entry point for the application.
 It initializes the program and starts the main functionality."""
@@ -22,7 +22,7 @@ def run_character_creator():
                 mf.custom_character(character)
 
                 # Prompt user to create another character or exit.
-                if yes_no("\n\nDO YOU WANT TO CREATE ANOTHER CHARACTER (Y/N)? "):
+                if check_yes_no("\n\nDO YOU WANT TO CREATE ANOTHER CHARACTER (Y/N)? "):
                     mf.show_menu()
                     continue
                 else:
@@ -33,7 +33,7 @@ def run_character_creator():
                 mf.random_character(character)
 
                 # Prompt user to create another character or exit.
-                if yes_no("\n\nDO YOU WANT TO CREATE ANOTHER CHARACTER (Y/N)? "):
+                if check_yes_no("\n\nDO YOU WANT TO CREATE ANOTHER CHARACTER (Y/N)? "):
                     mf.show_menu()
                     continue
                 else:
