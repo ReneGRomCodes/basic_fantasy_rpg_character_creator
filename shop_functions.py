347d36f04f64ec74fe19ff33d0799ba94c317a76
René Romero <rene.romero.2517@gmail.com>
2024-08-21 12:07:52 +0200

    changed string formatting in function 'show_shop()' in 'shop_functions.py'.

--- shop_functions.py
+++ shop_functions.py
@@ -4,6 +4,7 @@ import item_instances
 
 def show_shop():
     print("General Items:")
+    print(f"{"Weight":>40}{"Cost":>10}")
     for item in item_instances.general_items:
         print(f" - {item.name:<30}{f"{item.weight} lbs":>7}{f"{item.cost} gp":>10}")
 
@@ -12,18 +13,19 @@ def show_shop():
         print(k)
         if k == "Ranged Weapons":
             for item in v:
-                print(f" - {item.name:<20}{item.size}{f"{item.weight} lbs":>10}{item.cost:>18} gp")
+                print(f" - {item.name:<20}{item.size}{f"{item.weight} lbs":>10}{f"{item.cost} gp":>18}")
         else:
             for item in v:
-                print(f" - {item.name:<20}{item.size}{f"{item.weight} lbs":>10}{f"1d{item.damage}":>8}{item.cost:>10} gp")
+                print(f" - {item.name:<20}{item.size}{f"{item.weight} lbs":>10}{f"1d{item.damage}":>8}{f"{item.cost} gp":>10}")
 
     print("\nProjectiles:")
     for projectile in item_instances.projectiles:
         print(f" - {projectile.name}")
 
     print("\nArmor:")
+    print(f"{"Weight":>28}{"AC":>10}{"Cost":>10}")
     for armor in item_instances.armors:
-        print(f" - {armor.name}")
+        print(f" - {armor.name:<15}{f"{armor.weight} lbs":>10}{armor.armor_class:>10}{f"{armor.cost} gp":>10}")
 
 
 # show_shop()
