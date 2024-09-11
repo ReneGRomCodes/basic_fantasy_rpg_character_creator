94374048f630f313766b32c68347438c12d85dd6
René Romero <rene.romero.2517@gmail.com>
2024-09-11 12:19:15 +0200

    added TODO list to 'dev_scratch.py' for stuff that I'll get to later.

--- dev_scratch.py
+++ dev_scratch.py
@@ -1,1 +1,4 @@
 """This file contains code in development and is meant to try out concepts or just play around with ideas."""
+
+# TODO figure out how to implement different attacks with spear in 'item_instance.py'
+# TODO Equip/unequip methods work only for armor right now in 'character_model.py'
