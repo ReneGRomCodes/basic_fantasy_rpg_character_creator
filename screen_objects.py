0d2b89f2abfe72ddc9ee1afab8c66d5e4692d978
René Romero <rene.romero.2517@gmail.com>
2024-09-22 12:30:48 +0200

    refactored classes in module 'screen_objects.py'.

--- screen_objects.py
+++ screen_objects.py
@@ -2,27 +2,23 @@ import pygame
 """Classes for screen objects (buttons, text, etc.)."""
 
 
-class ScreenObject:
+class TextField:
+    """Represent field of text."""
 
-    def __init__(self, screen):
+    def __init__(self, screen, text, x=0, y=0):
+        """Initialize the text field with screen, text, and position."""
         self.screen = screen
         self.screen_rect = screen.get_rect()
-
-
-class TextField(ScreenObject):
-
-    def __init__(self, screen, text):
-        super().__init__(screen)
         self.text = text
+        self.x = x
+        self.y = y
+
+        # Set text color to black and get rect for text field.
         self.text_color = (0, 0, 0)
         self.font = pygame.font.SysFont(None, 48)
         self.text_image = self.font.render(self.text, True, self.text_color)
-
-        # Create a rect for positioning.
         self.text_rect = self.text_image.get_rect()
-        # Center text on the screen.
-        self.text_rect.centerx = self.screen_rect.centerx
-        self.text_rect.centery = self.screen_rect.centery
 
     def draw_text(self):
+        """Draw the text on the screen."""
         self.screen.blit(self.text_image, self.text_rect)
