ee12482bd89d2b545ea3de8076c7f0fa9299a13b
René Romero <rene.romero.2517@gmail.com>
2024-10-01 19:33:29 +0200

    implemented further functionality to class 'LabeledText' in 'screen_objects.py' for testing.

--- gui/screen_objects.py
+++ gui/screen_objects.py
@@ -5,18 +5,20 @@ import pygame
 class TextField:
     """Represent field of text."""
 
-    def __init__(self, screen, text, size):
+    def __init__(self, screen, text, size, bg_color=(0, 0, 0, 0)):
         """Initialize a text field on screen
         ARGS:
             screen: pygame window.
             text: string to be shown in text field.
             size: font size for text.
+            bg_color: background color for rect. Default is transparent.
         Default position is centered on screen.
         """
         self.screen = screen
         self.screen_rect = screen.get_rect()
         self.text = text
         self.size = size
+        self.bg_color = bg_color
 
         # Set text color to black and get rect for text field.
         self.text_color = (0, 0, 0)
@@ -28,22 +30,28 @@ class TextField:
         self.text_rect.center = self.screen_rect.center
 
     def draw_text(self):
-        """Draw the text on the screen."""
+        """Draw the text field on the screen."""
+        # Draw background rect if 'bg_color' is not transparent.
+        if self.bg_color != (0, 0, 0, 0):
+            pygame.draw.rect(self.screen, self.bg_color, self.text_rect)
+
+        # Draw the text on top of the rectangle
         self.screen.blit(self.text_image, self.text_rect)
 
 
 class Button(TextField):
     """Represent an interactive button."""
 
-    def __init__(self, screen, text, size):
+    def __init__(self, screen, text, size, bg_color=(0, 0, 0, 0)):
         """Initialize an interactive button on screen
         ARGS:
             screen: pygame window.
             text: string to be shown on the button.
             size: font size for text.
+            bg_color: background color for rect. Default is transparent.
         Default position is centered on screen.
         """
-        super().__init__(screen, text, size)
+        super().__init__(screen, text, size, bg_color)
         # Set button colors for events.
         self.rect_hover_color = (200, 200, 200)
         self.rect_clicked_color = (240, 240, 240)
@@ -55,6 +63,10 @@ class Button(TextField):
     def draw_button(self, mouse_pos):
         """Draw the button on the screen, changing color based on hover or click using 'mouse_pos' as initialized in
         main loop in 'main.py'."""
+        # Draw background rect if 'bg_color' is not transparent.
+        if self.bg_color != (0, 0, 0, 0):
+            pygame.draw.rect(self.screen, self.bg_color, self.button_rect)
+
         # Determine button color based on mouse hover or click.
         if self.button_rect.collidepoint(mouse_pos):
             if pygame.mouse.get_pressed()[0]:
@@ -70,15 +82,16 @@ class Button(TextField):
 class LabeledText(Button):
     """Represent an interactive text field with popup when mouse hovers over it. """
 
-    def __init__(self, screen, text, size):
+    def __init__(self, screen, text, size, bg_color=(0, 0, 0, 0)):
         """Initialize an interactive text field with popup on screen.
         ARGS:
             screen: pygame window.
             text: string to be shown for the text field.
             size: font size for text.
+            bg_color: background color for rect. Default is transparent.
         Default position is centered on screen.
         """
-        super().__init__(screen, text, size)
+        super().__init__(screen, text, size, bg_color)
         # NOTE: Rect, size and position (text_image, text_rect and text_rect.center) are set by attributes from class
         # 'TextField' not 'Button'.
 
