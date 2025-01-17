af5ea651ddca142342a5f72b1352c87618d64bd9
René Romero <rene.romero.2517@gmail.com>
2025-01-17 16:14:21 +0100

    created basic module with class 'CharacterSheet' to reorganize and declutter functions in module 'character_sheet.py'.

---
+++ gui/character_sheet/cs_model.py
@@ -0,0 +1,11 @@
+"""
+Helper class to organize and access character sheet objects as attributes.
+"""
+
+
+class CharacterSheet:
+    """A class to store and manage character sheet elements from the 'cs_elements' dictionary."""
+
+    def __init__(self, cs_elements):
+        """Initialize the CharacterSheet object with elements from the 'cs_elements' dictionary."""
+        pass
