f2351232ede7b3401389b77a7bb30767f181c707
René Romero <rene.romero.2517@gmail.com>
2025-01-21 14:04:44 +0100

    added saving throw objects to character sheet screen output.

--- gui/cs_model.py
+++ gui/cs_model.py
@@ -181,7 +181,18 @@ class CharacterSheet:
         # Saving throws group.
         # Using 'saving_throws_title' rect for reference and easier positioning.
         group_ref_rect = self.saving_throws_title.text_rect
+        # Group starting on 'x_column_1', 'y_row_3' by positioning 'saving_throws_title' first.
         group_ref_rect.top, group_ref_rect.left = y_row_3, x_column_1
+        # Position attribute for saving throw scores.
+        y_row_4 = group_ref_rect.bottom
+        for group in self.saving_throw_groups:
+            group[0].text_rect.top, group[0].text_rect.left = y_row_4, x_column_1
+            group[1].text_rect.top, group[1].text_rect.right = y_row_4, x_column_2
+            # Move row position down for next group.
+            y_row_4 = group[0].text_rect.bottom
+
+        # Reset 'y_row_4' to starting position.
+        y_row_4 = group_ref_rect.bottom
 
     def show_character_sheet_screen(self):
         """Draw character sheet elements on screen."""
@@ -217,6 +228,9 @@ class CharacterSheet:
             group[2].draw_text()
         # Draw saving throw fields.
         self.saving_throws_title.draw_text()
+        for group in self.saving_throw_groups:
+            group[0].draw_text()
+            group[1].draw_text()
 
     def format_ability_bonus_penalty(self):
         """Format output for 0/positive values of ability score's bonus and penalty. Remove value if it is '0' or add '+'
