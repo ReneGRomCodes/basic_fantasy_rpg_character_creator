8fc96e45c0bf364e4ffbd5e424b9f912c9d3e356
René Romero <rene.romero.2517@gmail.com>
2025-02-25 13:39:05 +0100

    got 'dev_scratch.py' in order.

--- dev_scratch.py
+++ dev_scratch.py
@@ -1,10 +1,15 @@
-"""This file contains code in development and is meant to try out concepts or just play around with ideas."""
+"""This file contains code in development and is meant to try out concepts or just play around with ideas... alright, at
+this point it has become mostly a dumping ground for 'todos'."""
 
+# Old TODOS from console times that could still be good to keep in mind when migrating that part over to PyGame:
 # TODO figure out how to implement different attacks with spear in 'item_instance.py'
 # TODO Equip/unequip methods work only for armor right now in 'character_model.py'
 # TODO fully implement weapons shop within 'shop_functions.py'
+
+# Shit to keep in mind:
 # TODO keep an eye on functions in 'core\functions.py' and 'character_creation_functions.py' that will be obsolete after migration.
 # TODO Remove function call 'build_character_sheet()' from event handler when final character sheet is done.
+# TODO pop-up texts overlap with underlying text on some screens.
 
 # PRIORITIES:
 # TODO refactor function 'show_ability_scores_screen' in 'gui/gui.py'.
