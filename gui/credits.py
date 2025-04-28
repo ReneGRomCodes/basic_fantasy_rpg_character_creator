0c33c5a2599af9b64acff3e73723446e3f8e07c2
René Romero <rene.romero.2517@gmail.com>
2025-04-28 09:22:49 +0200

    added type hints/annotations in 'gui/credits.py'.

--- gui/credits.py
+++ gui/credits.py
@@ -1,4 +1,5 @@
 import gui.screen_objects as so
+from gui.screen_objects import TextField
 from gui.ui_helpers import draw_screen_title
 from core.settings import settings
 """
@@ -9,43 +10,53 @@ Class for credits screen.
 class Credits:
     """Class to store, manage and show credits screen for pygame."""
 
-    def __init__(self, screen, gui_elements):
-        """Initialize credits screen elements."""
+    def __init__(self, screen, gui_elements: dict) -> None:
+        """Initialize credits screen elements.
+        ARGS:
+            screen: PyGame window.
+            gui_elements: dict of gui elements as created in module 'gui_elements.py'.
+        """
         # Assign text sizes from 'gui_elements' to attributes.
-        title_size, text_large, text_medium = gui_elements["title_size"], gui_elements["text_large"], gui_elements["text_medium"]
+        title_size: int = gui_elements["title_size"]
+        text_large: int = gui_elements["text_large"]
+        text_medium: int = gui_elements["text_medium"]
         # Positioning variables.
-        self.credits_pos_y_start = screen.get_rect().bottom + 5
-        self.title_pos_x = screen.get_rect().centerx + screen.get_rect().width / 12
-        self.name_pos_x = screen.get_rect().centerx
-        self.category_spacing = screen.get_rect().height / 10
+        self.credits_pos_y_start: int = screen.get_rect().bottom + 5
+        self.title_pos_x: int = screen.get_rect().centerx + screen.get_rect().width / 12
+        self.name_pos_x: int = screen.get_rect().centerx
+        self.category_spacing: int = screen.get_rect().height / 10
         # Y-position variable which dynamically changes for each element on screen.
-        self.dynamic_pos_y = self.credits_pos_y_start
+        self.dynamic_pos_y: int = self.credits_pos_y_start
 
         # Credits screen title.
-        self.credits_title = so.TextField(screen, "- CREDITS -", title_size)
+        self.credits_title: TextField = so.TextField(screen, "- CREDITS -", title_size)
         # Credits.
-        dev_title = so.TextField(screen, "Lead Developer", text_large)
-        dev_name = so.TextField(screen, "René Grewe Romero", text_medium)
-        concept_creator_title = so.TextField(screen, "Based on 'Basic Tabletop RPG'", text_large)
-        concept_creator_name = so.TextField(screen, "by Chris Gonnerman", text_medium)
-        art_ui_title = so.TextField(screen, "Artwork & UI Design", text_large)
-        art_ui_name = so.TextField(screen, "René Grewe Romero", text_medium)
-        font_creator_title = so.TextField(screen, "Font 'Eagle Lake'", text_large)
-        font_creator_name = so.TextField(screen, "by Brian J. Bonislawsky", text_medium)
+        dev_title: TextField = so.TextField(screen, "Lead Developer", text_large)
+        dev_name: TextField = so.TextField(screen, "René Grewe Romero", text_medium)
+        concept_creator_title: TextField = so.TextField(screen, "Based on 'Basic Tabletop RPG'", text_large)
+        concept_creator_name: TextField = so.TextField(screen, "by Chris Gonnerman", text_medium)
+        art_ui_title: TextField = so.TextField(screen, "Artwork & UI Design", text_large)
+        art_ui_name: TextField = so.TextField(screen, "René Grewe Romero", text_medium)
+        font_creator_title: TextField = so.TextField(screen, "Font 'Eagle Lake'", text_large)
+        font_creator_name: TextField = so.TextField(screen, "by Brian J. Bonislawsky", text_medium)
 
         # Array of objects to be shown on screen as instantiated above. Each inner tuple representing a credit category,
         # with the element at index [0] being the category title and the following elements being the credited names.
-        self.credits_elements = ((dev_title, dev_name),
-                                 (concept_creator_title, concept_creator_name),
-                                 (art_ui_title, art_ui_name),
-                                 (font_creator_title, font_creator_name))
+        self.credits_elements: tuple[tuple[TextField, TextField], ...] = ((dev_title, dev_name),
+                                                                          (concept_creator_title, concept_creator_name),
+                                                                          (art_ui_title, art_ui_name),
+                                                                          (font_creator_title, font_creator_name))
 
         # Calculate fade-out speed. Represent intervals for alpha value changes per frame when credits fade out at the
         # top of the screen.
-        self.fading_speed = int(7 * (30 / settings.frame_rate))
+        self.fading_speed: int = int(7 * (30 / settings.frame_rate))
 
-    def show_credits(self, screen, gui_elements):
-        """Position and draw credits on screen."""
+    def show_credits(self, screen, gui_elements) -> None:
+        """Position and draw credits on screen.
+        ARGS:
+            screen: PyGame window.
+            gui_elements: dict of gui elements as created in module 'gui_elements.py'.
+        """
 
         draw_screen_title(screen, self.credits_title, gui_elements)
 
@@ -73,8 +84,11 @@ class Credits:
         # Call function to have credits scroll from bottom to top over the screen.
         self.dynamic_credits_position(screen)
 
-    def dynamic_credits_position(self, screen):
-        """Dynamically change position of credits elements for scrolling effect."""
+    def dynamic_credits_position(self, screen) -> None:
+        """Dynamically change position of credits elements for scrolling effect.
+        ARGS:
+            screen: PyGame window.
+        """
         # Change starting y-position for each frame and set new starting value for dynamically changed y-position to
         # make text move across the screen.
         self.credits_pos_y_start -= screen.get_rect().height / 300
@@ -85,9 +99,13 @@ class Credits:
             self.credits_pos_y_start = screen.get_rect().bottom + 5
             self.dynamic_pos_y = self.credits_pos_y_start
 
-    def fade_out_credits(self, screen, item):
+    def fade_out_credits(self, screen, item: TextField) -> None:
         """Fade out credits item when it reaches top of the screen, then reset alpha transparency to opaque when it has
-        fully left the screen."""
+        fully left the screen.
+        ARGS:
+            screen: PyGame window.
+            item: 'TextField' instance.
+        """
         # Set item transparency based on position on screen for fade-out effect.
         if item.text_rect.top <= screen.get_rect().height / 4 and not item.text_rect.bottom <= screen.get_rect().top:
             item.text_surface.set_alpha(item.background_alpha)
